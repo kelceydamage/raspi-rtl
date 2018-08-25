f399ab4e74c52d6bef60d61e1ff5cfe4fd94c5da
kelcey Jamison-Damage <kelceydamage@users.noreply.github.com>
2018-08-24 22:25:49 -0700

    Create start.sh

---
+++ transport/bin/start.sh
@@ -0,0 +1,33 @@
+echo -en "\n   +  "
+for i in {0..23}; do
+  printf "%2b " $i
+done
+
+#printf "\n\n %3b  " 0
+#for i in {0..15}; do
+#  echo -en "\033[48;5;${i}m  \033[m "
+#done
+
+#for i in 16 52 88 124 160 196 232; do
+for i in {0..5}; do
+  let "i = i*36 +16"
+  printf "\n\n %3b  " $i
+  for j in {0..23}; do
+    let "val = i+j"
+    echo -en "\033[48;5;${val}m  \033[m "
+  done
+done
+
+echo -e "\n"
+echo -e "         R A S P I      (c) Robot OS                         -K. Damage\n"
+
+local_home=`pwd`
+export PATH=$local_home:$PATH
+export RASPI_HOME=/git/projects/cython/personal
+#export RASPI_HOME=/opt/nvme
+#export RASPI_HOME=/opt
+PYTHON="python"
+$PYTHON $RASPI_HOME/raspi/transport/bin/start.py -m
+$PYTHON $RASPI_HOME/raspi/transport/bin/start.py ROUTER -a 0.0.0.0 &
+$PYTHON $RASPI_HOME/raspi/transport/bin/start.py CACHE -a 0.0.0.0 &
+$PYTHON $RASPI_HOME/raspi/transport/bin/start.py TASK -a 0.0.0.0 -p 19100 &
