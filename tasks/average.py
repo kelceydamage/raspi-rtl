5b645cbbe930544e12e970e3c9631e5065478209
Kelcey Jamison-Damage <kdamage@palantir.com>
2018-08-25 14:26:14 -0700

    removed uneeded boiler plate and updated logging

---
+++ tasks/average.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# ------------------------------------------------------------------------ 79->
+# Author: ${name=Kelcey Damage}
+# Python: 3.5+
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Doc
+# ------------------------------------------------------------------------ 79->
+
+# Imports
+# ------------------------------------------------------------------------ 79->
+from common.print_helpers import Logger
+from transport.conf.configuration import LOG_LEVEL
+
+# Globals
+# ------------------------------------------------------------------------ 79->
+LOG = Logger(LOG_LEVEL)
+
+# Classes
+# ------------------------------------------------------------------------ 79->
+
+# Functions
+# ------------------------------------------------------------------------ 79->
+def task_get_average(kwargs):
+    name = 'NODE-{0}'.format(kwargs['worker'])
+    LOG.logc(name, 'starting task', 'end(null)', 1, 'LIGHTBLUE')
+    results = []
+    for item in kwargs['data']:
+        results.append(float(sum(item)) / float(len(item)))
+    return results
+# Main
+# ------------------------------------------------------------------------ 79->
