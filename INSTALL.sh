e7e29c7b5573e7e29be8068b62900c38927598d9
Kelcey Jamison-Damage <kdamage@palantir.com>
2018-08-25 22:35:31 -0700

    added ghetto installer

--- INSTALL.sh
+++ INSTALL.sh
@@ -13,14 +13,18 @@ if [ $ID == ubuntu ]
 then
     sudo apt-get install perl wheel build-essential libzmq3-dev
     sudo apt-get install python3-setuptools python3-dev python3-pip 
+
+    pip3 install virtualenv
+    virtualenv -p $(which python3) ~/python3
+    source ~/python3/bin/activate
 else
     sudo yum install python36u-pip python36u-devel python36u-setuptools
     sudo yum install zeromq-devel-4.1.4-5.el7.x86_64 zeromq-4.1.4-5.el7.x86_64
-fi
 
-pip3 install virtualenv
-virtualenv -p $(which python3) ~/python3
-source ~/python3/bin/activate
+    pip3.6 install virtualenv
+    virtualenv -p $(which python3) ~/python36
+    source ~/python36/bin/activate
+fi
 
 pip install ujson
 pip install pyzmq
