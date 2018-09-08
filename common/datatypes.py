49d87d81cac48197a1237b667f6a45a75d847278
Kelcey Jamison-Damage <kdamage@palantir.com>
2018-09-07 22:45:19 -0700

    complete rewrite. Major overhead reduction, and phasing out other support classes: Pipeline(), Meta()

--- common/datatypes.py
+++ common/datatypes.py
@@ -18,68 +18,28 @@
 # Doc
 # ------------------------------------------------------------------------ 79->
 # Dependancies:
-#                   hashlib
-#                   base64
-#                   ujson
-#                   collections
-#                   uuid
-#                   sys
-#                
+#                   cache
+#                   encoding
+#
 # Imports
 # ------------------------------------------------------------------------ 79->
-import hashlib
-import base64
-import ujson as json
-import collections
-import uuid
-import sys
+from transport.cache import Cache
+from common.encoding import Tools
 
 # Globals
 # ------------------------------------------------------------------------ 79->
+VERSION = '0.3'
+
 
 # Classes
 # ------------------------------------------------------------------------ 79->
-class Tools(object):
-    """
-    NAME:           Tools
-
-    DESCRIPTION:    A class of utility functions used by the datatypes.
-
-    METHODS:        .serialize(obj)
-                    Converts an object into a bytes encoded serialized object.
-
-                    .deserialize(obj)
-                    Converts a bytes encoded serialized object back into an 
-                    object.
-
-                    .create_header(meta)
-                    Creates a bytes encoded hash(md5) of the metadata.
-
-                    .create_id()
-                    Creates a bytes encoded UUID4.
-    """
-    @staticmethod
-    def serialize(obj):
-        return base64.b64encode((json.dumps(obj)).encode())
-
-    @staticmethod
-    def deserialize(obj):
-        return json.loads(base64.b64decode(obj).decode())
-
-    @staticmethod
-    def create_header(meta):
-        return hashlib.md5(json.dumps(meta).encode()).hexdigest().encode()
-
-    @staticmethod
-    def create_id():
-        return str(uuid.uuid4()).encode()
 
 class Parcel(object):
     """
     NAME:           Envelope
 
-    DESCRIPTION:    A class of utility functions used by the envelope when 
-                    routing, beit dealer or router sockets. It stores an 
+    DESCRIPTION:    A class of utility functions used by the envelope when
+                    routing, beit dealer or router sockets. It stores an
                     additional header(route) and a null byte char. Used for
                     sending between routing members of the transport framework.
 
@@ -90,7 +50,7 @@ class Parcel(object):
                     Unpacks the parcel into both a route and an envelope.
 
                     .load()
-                    Loads the serialized representation of a parcel into a 
+                    Loads the serialized representation of a parcel into a
                     parcel(obj).
 
                     .seal()
@@ -118,134 +78,246 @@ class Parcel(object):
     def seal(self):
         return list(self.contents)
 
+
 class Envelope(object):
     """
-    NAME:           Envelope
+    NAME:           Envelope v0.3
 
-    DESCRIPTION:    A class of utility functions used by the datatypes. It 
+    DESCRIPTION:    A class of utility functions used by the datatypes. It
                     stores a header, meta, pipeline, and data, for sending
                     between members of the transport framework.
 
-    METHODS:        .pack(header, meta, pipeline, data)
-                    Packs up a header, meta(obj), pipeline(obj), and data
+    METHODS:        ._compression()
+                    Check for compression flag or return False.
+
+                    ._flatten(obj)
+                    If compressed then uncompress.
+
+                    ._compress(obj)
+                    If not compressed then compress.
+
+                    ._set_params()
+                    Assign specific instance attributes from meta.
+
+                    ._complete_meta
+                    Assing specific meta attributes packed content.
+
+                    .open(compressed=False)
+                    Returns the envelope(obj) in it's completely uncompressed
+                    form. If compressed is True, return the contents without
+                    decompressing them.
+
+                    .seal()
+                    Returns the content as a list of bytes objects for
+                    transmission.
+
+                    .cache_data()
+                    Cache the data content.
+
+                    .retrieve_cache()
+                    Retrieve cached data content.
+
+                    .check_for_cached_data()
+                    Check to see if data content is cached.
+
+                    .pack(header, meta, pipeline, data)
+                    Packs up a header, meta, pipeline, and data
                     into and envelope(obj).
 
                     .load(sealed_envelope)
-                    Loads the serialized representation of an envelope(obj)
-                    into an envelope(obj).
+                    Loads a compressed envelope right off the wire into an
+                    envelope object.
 
-                    .open()
-                    Returns the envelope(obj) in it's completely deserialized
-                    form.
+                    .get_length()
+                    Returns the envelopes length in either raw or compressed
+                    form based on the self.compressed flag.
 
-                    .unpack()
-                    Unpacks the envelope(obj) into a header, meta(obj), 
-                    pipeline(obj), and data.
-
-                    .seal()
-                    Returns the internal deque as a list.
+                    .get_lifespan()
+                    Returns the envelopes lifespan in either raw or compressed
+                    form based on the self.compressed flag.
 
                     .get_header()
-                    Returns bytes encoded serialized header.
-
-                    .get_raw_header()
-                    Returns the deserialized header.
+                    Returns the envelopes header in either raw or compressed
+                    form based on the self.compressed flag.
 
                     .get_meta()
-                    Returns the envelopes deserialized meta(obj).
+                    Returns the envelopes meta in either raw or compressed
+                    form based on the self.compressed flag.
 
                     .get_pipeline()
-                    Returns the envelopes deserialized pipeline(obj).
+                    Returns the envelopes pipeline in either raw or compressed
+                    form based on the self.compressed flag.
 
                     .get_data()
-                    Returns the envelopes deserialized data.
-
-                    .update_data(data)
-                    Replace the envelopes data.
-
-                    .update_meta(meta)
-                    Replace the envelopes meta.
-
-                    .empty()
-                    Clear the envelope.
+                    Returns the envelopes data in either raw or compressed
+                    form based on the self.compressed flag.
 
                     .validate()
-                    Validate the envelope length.
+                    Inactive.
     """
-    def __init__(self):
-        self.contents = collections.deque(maxlen=4)
-        self.lifespan = 0
+    __slots__ = [
+        'header',
+        'meta',
+        'pipeline',
+        'data',
+        'manifest',
+        'cached',
+        'compressed',
+        'cache',
+        'length',
+        'lifespan'
+        ]
+
+    def __init__(self, cached=False):
+        self.compressed = False
+        self.cached = cached
+        self.manifest = ['header', 'meta', 'pipeline', 'data']
+        self.cache = Cache()
+
+    def __getattr__(self, key):
+        if self._compression() and key in self.manifest:
+            return self._compress(object.__getattribute__(self, key))
+        else:
+            return self._flatten(object.__getattribute__(self, key))
+
+    def __setattr__(self, key, value):
+        if self._compression() and key in self.manifest:
+            object.__setattr__(self, key, self._compress(value))
+        else:
+            object.__setattr__(self, key, self._flatten(value))
+
+    def __len__(self):
+        return self.length
+
+    def __getitem__(self, key):
+        if key not in self.__slots__:
+            return self.__missing__(key)
+        return self.__getattr__(key)
+
+    def __missing__(self, key):
+        return False
+
+    def __setitem__(self, key, value):
+        self.__setattr__(key, value)
+
+    def __delitem__(self, key):
+        pass
+
+    def __contains__(self, key):
+        if key in self.__slots__:
+            return True
+        return False
+
+    def _compression(self):
+        try:
+            return object.__getattribute__(self, 'compressed')
+        except AttributeError:
+            return False
+
+    def _flatten(self, obj):
+        if not isinstance(obj, bytes):
+            return obj
+        return Tools.deserialize(obj)
+
+    def _compress(self, obj):
+        if isinstance(obj, bytes):
+            return obj
+        elif isinstance(obj, Cache):
+            return obj
+        else:
+            return Tools.serialize(obj)
+
+    def _set_params(self):
+        meta = self.get_meta()
+        self.length = meta['length']
+        self.lifespan = meta['lifespan']
+
+    def _complete_meta(self):
+        self.meta['lifespan'] = len(self.pipeline['tasks'])
+        self.meta['length'] = len(self.data)
+        self._set_params()
+
+    def open(self, compressed=False):
+        self.compressed = compressed
+        return [self[k] for k in self.manifest]
+
+    def seal(self):
+        self.compressed = True
+        if self.cached:
+            self.cache_data()
+        return self.open(True)
+
+    def cache_data(self):
+        key = Tools.create_key(Tools.create_id())
+        r = self.cache.put(key, self['data'])
+        if r[1]:
+            self['meta']['cache_key'] = key
+            self['data'].clear()
+
+    def retrieve_cache(self):
+        key = self['meta']['cache_key']
+        r = self.cache.get(key)
+        self.update_data(r[1])
+        self['meta']['spent_key'] = key
+        self['meta']['cache_key'] = None
+
+    def check_for_cached_data(self):
+        if self['meta']['cache_key'] is not None:
+            self.retrieve_cache()
 
     def pack(self, header, meta, pipeline, data):
-        meta['lifespan'] = len(pipeline['tasks'])
-        meta['length'] = len(data)
-        meta['size'] = sys.getsizeof(data)
-        self.lifespan = meta['lifespan']
-        self.length = meta['length']
-        self.size = meta['size']
-        if header != '':
-            h = header
-        else:
-            h = Tools.serialize(Tools.create_id())
-        [self.contents.append(Tools.serialize(x)) for x in (h, meta, pipeline, data)]
+        self.compressed = False
+        if header == '':
+            header = Tools.create_id()
+        self.__setattr__('header', header)
+        self.__setattr__('meta', meta)
+        self.check_for_cached_data()
+        self.__setattr__('pipeline', pipeline)
+        self.__setattr__('data', data)
+        self._complete_meta()
         del header
         del meta
         del pipeline
         del data
-        
+
     def load(self, sealed_envelope):
-        [self.contents.append(x) for x in sealed_envelope]
-        meta = self.get_meta()
-        self.lifespan = meta.lifespan
-        self.length = meta.length
-        self.size = meta.size
+        self.compressed = True
+        self.pack(
+            sealed_envelope[0],
+            sealed_envelope[1],
+            sealed_envelope[2],
+            sealed_envelope[3]
+        )
+        self._set_params()
         del sealed_envelope
-        del meta
 
-    def open(self):
-        return [Tools.deserialize(x) for x in self.contents]
+    def get_length(self):
+        return self._flatten(self.length)
 
-    def unpack(self):
-        return self.get_raw_header(), self.get_meta(), self.get_pipeline(), self.get_data()
-
-    def seal(self):
-        return list(self.contents)
+    def get_lifespan(self):
+        return self._flatten(self.lifespan)
 
     def get_header(self):
-        return self.contents[0]
-
-    def get_raw_header(self):
-        return Tools.deserialize(self.contents[0])
+        return self._flatten(self.header)
 
     def get_meta(self):
-        return Meta(Tools.deserialize(self.contents[1]))
+        return self._flatten(self.meta)
 
     def get_pipeline(self):
-        return Pipeline(Tools.deserialize(self.contents[2]))
+        return self._flatten(self.pipeline)
 
     def get_data(self):
-        return Tools.deserialize(self.contents[-1])
-
-    def update_data(self, data):
-        self.contents.pop()
-        self.contents.append(Tools.serialize(data))
-
-    def update_meta(self, meta):
-        self.contents.pop(1)
-        self.contents.insert(Tools.serialize(meta.extract()), 1)
-
-    def empty(self):
-        self.contents.clear()
+        return self._flatten(self.data)
 
     def validate(self):
-        if len(self.contents) != 4:
-            raise Exception('[ENVELOPE] (validation): size missmatch')
+        pass
+
 
 class Pipeline(object):
     """
     NAME:           Pipeline
 
-    DESCRIPTION:    A class for storing the envelopes pipeline. Tasks to be 
+    DESCRIPTION:    A class for storing the envelopes pipeline. Tasks to be
                     completed, tasks already completed, and task arguments
                     are part of the pipeline object.
 
@@ -262,9 +334,9 @@ class Pipeline(object):
     """
     def __init__(self, pipeline=None):
         self.kwargs = {}
-        self.tasks = collections.deque()
-        self.completed = collections.deque()
-        if pipeline != None:
+        self.tasks = []
+        self.completed = []
+        if pipeline is not None:
             self.load(pipeline)
 
     def extract(self):
@@ -283,6 +355,7 @@ class Pipeline(object):
         self.completed.append(current)
         return current
 
+
 class Meta(object):
     """
     NAME:           Meta
@@ -299,21 +372,23 @@ class Meta(object):
                     meta(obj).
     """
     def __init__(self, meta=None):
-        self.size = 0
         self.length = 0
         self.lifespan = 0
         self.times = {}
         self.stage = Tools.create_id()
-        if meta != None:
+        self.cache_key = None
+        self.spent_key = None
+        if meta is not None:
             self.load(meta)
 
     def extract(self):
         return {
-            'size': self.size,
             'length': self.length,
             'lifespan': self.lifespan,
             'times': self.times,
-            'stage': self.stage
+            'stage': self.stage,
+            'cache_key': self.cache_key,
+            'spent_key': self.spent_key
         }
 
     def load(self, meta):
