667a00ef1a83e9119aa409833c7c58ffdbca61f3
Kelcey Jamison-Damage <kdamage@palantir.com>
2018-09-08 00:35:17 -0700

    removed garbage

--- jobs/dummy_cache.py
+++ jobs/dummy_cache.py
@@ -28,13 +28,10 @@ os.sys.path.append(
             )
         )
     )
-from transport.dispatch import Cache
-from common.datatypes import Tools
-from transport.conf.configuration import CACHE_PATH
+from transport.cache import Cache
 
 # Globals
 # ------------------------------------------------------------------------ 79->
-HEADER = Tools.create_id()  # pragma: no cover
 CACHE = Cache()  # pragma: no cover
 
 # Classes
@@ -44,70 +41,18 @@ CACHE = Cache()  # pragma: no cover
 # ------------------------------------------------------------------------ 79->
 
 
-def test_cache_check():  # pragma: no cover
-    print('TEST: (check)')
-    assert str(CACHE.send('check', HEADER)[1]) == 'False'
-
-
-def test_cache_set():  # pragma: no cover
-    print('TEST: (set)')
-    assert str(CACHE.send('set', HEADER, 'test data')[1]) == 'True'
-
-
-def test_cache_get():  # pragma: no cover
-    print('TEST: (get)')
-    assert CACHE.send('get', HEADER)[1] == 'test data'
-
-
 def test_cache_status():  # pragma: no cover
-    print('TEST: (status)')
-    assert type(CACHE.send('status')[1]) == dict
-    assert list(CACHE.send('status')[1].keys()) == [
-        'psize',
-        'depth',
-        'branch_pages',
-        'leaf_pages',
-        'overflow_pages',
-        'entries'
-        ]
+    return CACHE.status()
 
 
 def test_cache_info():  # pragma: no cover
-    print('TEST: (info)')
-    assert type(CACHE.send('info')[1]) == dict
-    assert list(CACHE.send('info')[1].keys()) == [
-        'map_addr',
-        'map_size',
-        'last_pgno',
-        'last_txnid',
-        'max_readers',
-        'num_readers'
-        ]
-
-
-def test_cache_stale():  # pragma: no cover
-    print('TEST: (stale)')
-    assert type(CACHE.send('stale_readers')[1]) == int
-
-
-def test_cache_readers():  # pragma: no cover
-    print('TEST: (path)')
-    assert CACHE.send('path')[1] == CACHE_PATH
-
-
-def test_cache_locks():  # pragma: no cover
-    print('TEST: (locks)')
-    assert type(CACHE.send('locks')[1]) == str
+    return CACHE.info()
 
 
 # Main
 # ------------------------------------------------------------------------ 79->
 if __name__ == '__main__':  # pragma: no cover
-    test_cache_check()
-    test_cache_set()
-    test_cache_get()
-    test_cache_status()
-    test_cache_info()
-    test_cache_stale()
-    test_cache_readers()
-    test_cache_locks()
+    print('STATUS:')
+    print(test_cache_status())
+    print('INFO:')
+    print(test_cache_info())
