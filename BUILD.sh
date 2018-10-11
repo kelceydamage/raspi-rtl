084e32eddc0f3c7c488aea6dd52ee7ab2ec5c78c
kelcey Jamison-Damage <kelceydamage@users.noreply.github.com>
2018-10-11 15:44:33 -0700

    Update BUILD.sh
    revamp build script. Tasted to resolve pathing issues on Ubuntu 18 (AGX Xavier)

--- BUILD.sh
+++ BUILD.sh
@@ -1,13 +1,32 @@
 # setenv
-source ~/python36/bin/activate
+source ~/python3/bin/activate
+
+# If GIT_HOME is not set to the root location of your git folder globally, you can set here.
+# GIT_HOME=~/git
+
+# Project/repo folder within your git home dir.
+PROJECT_DIR=rtl
+
+# Pre compile function for writing clean cpp file.
+function pre_compile() {
+    cython -a --cplus $1 --force --include-dir $GIT_HOME/$PROJECT_DIR
+}
+
+PYX_FILES=(
+    common/datatypes.pyx
+    common/encoding.pyx
+    common/print_helpers.pyx
+    transport/relay.pyx
+    transport/node.pyx
+    transport/dispatch.pyx
+    )
 
 # precompile c++ modules
-cython -a --cplus common/datatypes.pyx --force
-cython -a --cplus common/encoding.pyx --force
-cython -a --cplus common/print_helpers.pyx --force
-cython -a --cplus transport/relay.pyx --force
-cython -a --cplus transport/node.pyx --force
-cython -a --cplus transport/dispatch.pyx --force
+for i in ${PYX_FILES[@]}; do
+    echo -e "Pre-compiling: $i"
+    pre_compile $i
+done
 
 # general compile of project modules
+echo -e "Starting full compile"
 python setup.py build_ext --inplace --force
