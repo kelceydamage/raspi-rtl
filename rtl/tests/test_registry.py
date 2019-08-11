c53b68c7a8dbdda859c2e0983fcaa7e3c1ca7aba
Kelcey Jamison-Damage <kdamage@palantir.com>
2019-08-10 19:09:33 -0700

    fixed: bad import and method

--- rtl/tests/test_registry.py
+++ rtl/tests/test_registry.py
@@ -1,14 +1,9 @@
 """Test case for registry.py"""
-import sys
 from rtl.transport.registry import import_tasks
-from rtl.transport.registry import load_tasks
 
 
 def test_registry():
     """Test registry.py"""
-    python_path = [
-        x for x in sys.path if 'python3' in x and 'site' in x
-    ]
 
     # Test valid module name
     result = import_tasks('rtl.tasks.*')
@@ -16,8 +11,4 @@ def test_registry():
 
     # Test invalid module name
     result = import_tasks('bob.*')
-    assert isinstance(result, dict)
-
-    # Test valid task folder
-    result = load_tasks('{0}/rtl/tasks'.format(python_path[0]))
     assert isinstance(result, dict)
