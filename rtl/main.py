35cb5d95cef95bf99b886acdc6ff62cdb64b6cc0
Kelcey Jamison-Damage <kdamage@palantir.com>
2019-08-13 15:29:15 -0700

    refactored into class and improved docstrings

--- rtl/main.py
+++ rtl/main.py
@@ -24,15 +24,13 @@ Note:
     This script should only be run from the main raspi-rtl shell script.
 
 Attributes:
-    RUNDIR (string): path to pidfiles.
-        Default: (~/var/run)
     COLOURS (object): class of terminal colours.
-    NODES (dict): A mapping of string keywords to node classes.
-    COUNTS (dict): A mapping of string keywords to node counts.
+    NODES (list): A list of node objects containing a class reference and a
+        count.
 
 Todo
-    * Find a better way to handle args in the main function (less cyclomatic \
-        complexity).
+    * Find a better way to handle args in the ``run`` function (less \
+        cyclomatic complexity).
 
 """
 
@@ -54,18 +52,21 @@ from rtl.common.logger import log
 
 # Const
 # ------------------------------------------------------------------------ 79->
-RUNDIR = os.path.expanduser(conf.PIDFILES)
 COLOURS = Colours()
-NODES = {
-    'relay': Relay,
-    'task': TaskNode,
-    'plot': PlotNode
-}
-COUNTS = {
-    'relay': 1,
-    'task': conf.TASK_WORKERS,
-    'plot': conf.PLOT_WORKERS
-}
+NODES = [
+    {
+        'class': Relay,
+        'count': 1
+    },
+    {
+        'class': TaskNode,
+        'count': conf.TASK_WORKERS
+    },
+    {
+        'class': PlotNode,
+        'count': conf.PLOT_WORKERS
+    }
+]
 
 
 # Classes
@@ -77,110 +78,182 @@ class StartError(Exception):
         super(StartError, self).__init__()
         self.error = error
 
+class Launcher(object):
+    """Launcher is class for handling all the setup logic for running an RTL
+    cluster.
+
+    Attributes:
+        nodes (list): A list of node objects defined in the beginning of this
+            module/
+        conf (module): The configuration module.
+        parser (class): ArgumentParser class object.
+        args (class): Namespace class object:
+
+            - meta (bool): True to print meta, else False to run script.
+            - no_server (bool): Default to False, pass True for tests.
+        functions (dict): A dict of task modules.
+
+    """
+    def __init__(self):
+        """Initializer for the ``Launcher`` class.
+
+        """
+        self.nodes = NODES
+        self.conf = conf
+        self.parser = argparse.ArgumentParser(prog="Task Engine")
+        self.parser.add_argument(
+            '-m',
+            "--meta",
+            action="store_true",
+            default=False,
+            help="Print meta header"
+            )
+        self.parser.add_argument(
+            '-n',
+            "--no-server",
+            action="store_true",
+            default=False,
+            help="do not start server loop"
+            )
+        self.args = self.parser.parse_known_args()[0]
+        self.functions = import_tasks(self.conf.TASK_LIB)
+        self._store_pid()
+
+    def _print_meta(self):
+        """Print the loaded tasks to the console.
+
+        Warning:
+            This is a private member and should never be called directly.
+
+        Raises:
+            SystemExit: natural termination point for program.
+
+        """
+        print('-' * 79)
+        print('REGISTERED-TASKS:')
+        print('-' * 79)
+        for key in self.functions.keys():
+            print(' {0}{1}__{2} {3}{4}{2}'.format(
+                COLOURS.BCYAN,
+                COLOURS.BLACK,
+                COLOURS.ENDC,
+                COLOURS.LIGHTBLUE,
+                key
+                ))
+        print('-' * 79)
+        raise SystemExit
+
+    def _launch(self, service_class, count):
+        """Call startNode on each service and check for exceptions.
+
+        Warning:
+            This is a private member and should never be called directly.
+
+        Args:
+            service_class (ClassType): The name of a valude service class.
+            count (int): the number of service_class instances to start.
+
+        Returns:
+            bool: True if all services launched successfully, False otherwise.
+
+        """
+        try:
+            self._start_node(service_class, count)
+        except StartError:
+            return False
+        return True
+
+    def _start_node(self, service_class, count):
+        """Start a subprocess for each micro-service
+
+        Warning:
+            This is a private member and should never be called directly.
+
+        Args:
+            service_class (ClassType): The name of a valude service class.
+            count (int): the number of service_class instances to start.
+
+        """
+        if count == 0:
+            raise StartError('Invalid count, can not launch 0 services')
+        for _ in range(count):
+            process = Process(
+                target=_service_wrapper,
+                args=[service_class]
+            )
+            process.daemon = True
+            process.start()
+
+    def _store_pid(self):
+        """store_pid function for storing the scripts pid
+
+        Warning:
+            This is a private member and should never be called directly.
+
+        Note
+            This method writes a pidfile to ``self.conf.PIDFILES`` containing
+            the pid value.
+
+        """
+        pid = os.getpid()
+        rundir = os.path.expanduser(self.conf.PIDFILES)
+        with open('{0}{1}-{2}'.format(rundir, 'master', pid), 'w+') as file:
+            file.write(str(pid))
+
+
+    def _keep_alive(self):
+        """serve function for executing the main process loop
+
+        Warning:
+            This is a private member and should never be called directly.
+
+        Raises:
+            SystemExit: natural termination point for program if testing flag
+                ``no_server`` is True.
+
+        """
+        if self.args.no_server:
+            raise SystemExit('Exited for code testing')
+        while True:
+            time.sleep(1000)
+
+    def run(self):
+        """Main loop for lanching services. This is the main entry point for
+        running the CLI.
+
+        Hint:
+            This is the main entry point and how you should interact with this
+            class
+
+        Example:
+            .. code-block:: Python
+
+                Launcher().run()
+
+        Raises:
+            SystemExit: if any services fail to start.
+
+        """
+        if self.args.meta:
+            self._print_meta()
+        for service in self.nodes:
+            log('Launching {0}'.format(service['class']))
+            success = self._launch(
+                service['class'],
+                service['count']
+            )
+        if not success:
+            raise SystemExit('Failed to launch one or more micro-services')
+        self._keep_alive()
+
 
 # Functions
 # ------------------------------------------------------------------------ 79->
-def embedded_parser():
-    """This function collects and parses arguments passed to the main function.
-
-    Returns:
-        Namespace: A set of named values.
-
-    """
-    parser = argparse.ArgumentParser(prog="Task Engine")
-    group = parser.add_argument_group('Extras')
-    group.add_argument(
-        '-m',
-        "--meta",
-        action="store_true",
-        default=False,
-        help="Print meta header"
-        )
-    group.add_argument(
-        '-n',
-        "--no-server",
-        action="store_true",
-        default=False,
-        help="do not start server loop"
-        )
-    return parser.parse_known_args()[0]
-
-
-def print_meta(functions):
-    """Print the loaded tasks to the console.
-
-    Args:
-        functions (dict): The dict of task modules the platform has loaded
-            into memory and stored as a reference.
-
-    """
-    print('-' * 79)
-    print('REGISTERED-TASKS:')
-    print('-' * 79)
-    for key in functions.keys():
-        print(' {0}{1}__{2} {3}{4}{2}'.format(
-            COLOURS.BCYAN,
-            COLOURS.BLACK,
-            COLOURS.ENDC,
-            COLOURS.LIGHTBLUE,
-            key
-            ))
-    print('-' * 79)
-
-
-def launcher():
-    """Main loop for lanching services.
-
-    Returns:
-        bool: True if all services launched successfully, False otherwise.
-
-    """
-    for service in COUNTS:
-        log('Launching {0}'.format(service.upper()))
-        success = launch(NODES[service], COUNTS[service])
-    if not success:
-        print('Failed to launch one or more micro-services')
-        exit(1)
-    return success
-
-
-def launch(service_class, count):
-    """Call startNode on each service and check for exceptions.
-
-    Args:
-        service_class (ClassType): The name of a valude service class.
-
-    Returns:
-        bool: True if all services launched successfully, False otherwise.
-
-    """
-    try:
-        start_node(service_class, count)
-    except StartError:
-        return False
-    return True
-
-
-def start_node(service_class, count):
-    """Start a subprocess for each micro-service
-
-    Args:
-        service_class (ClassType): The name of a valude service class.
-
-    """
-    if count == 0:
-        raise StartError('Invalid count, can not launch 0 services')
-    for _ in range(count):
-        process = Process(
-            target=service_wrapper,
-            args=[service_class]
-        )
-        process.daemon = True
-        process.start()
-
-
-def service_wrapper(service_class):
+def _service_wrapper(service_class):
     """Call start on the the service inside the process.
+
+    Warning:
+        This is a private member and should never be called directly.
 
     Args:
         service_class (ClassType): The name of a valude service class.
@@ -194,51 +267,8 @@ def service_wrapper(service_class):
     except Exception as error:
         raise StartError(error)
 
-
-def store_pid():
-    """store_pid function for storing the scripts pid"""
-    pid = os.getpid()
-    with open('{0}{1}-{2}'.format(RUNDIR, 'master', pid), 'w+') as file:
-        file.write(str(pid))
-
-
-def serve(args):
-    """serve function for executing the main process loop
-
-    Args:
-        args (Namespace):
-            * meta (bool): True to print meta, else False to run script.
-            * no_server (bool): Default to False, pass True for tests.
-
-    """
-    if args.no_server:
-        quit(1)
-    while True:
-        time.sleep(1000)
-
-
-def main(args):
-    """Main function and entrypoint
-
-    Args:
-        args (Namespace):
-            * meta (bool): True to print meta, else False to run script.
-            * no_server (bool): Default to False, pass True for tests.
-
-    """
-    functions = import_tasks(conf.TASK_LIB)
-    if args.meta:
-        print_meta(functions)
-        exit(1)
-    print('Starting RTL')
-    launcher()
-    store_pid()
-    serve(args)
-
-
 # Main
 # ------------------------------------------------------------------------ 79->
 if __name__ == '__main__':  # pragma: no cover
-    # pass the embedded parser to the main function. This pattern is chosen to
-    # make testing easier.
-    main(embedded_parser())
+    # Call the Launcher class and invoke the run method.
+    Launcher().run()
